7bc8d1114c9901fafc1d63af104ca545187e51e6
Jackie He <Jackie.He8@gmail.com>
2018-02-12 22:18:19 +0000

    Day One

---
+++ weather-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './Router';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Weather Galore</h1>
+        </header>
+         <p className="App-intro"></p>
+        <BasicExample />
+      </div>
+    );
+  }
+}
+
+export default App;
